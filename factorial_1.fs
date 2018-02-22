d4e72929954b1a5d4e8397131f25defbb0bb7e82
Ivan Yarkov <ivan.yarkov@outlook.com>
2018-02-22 22:34:37 +0300

    Add files via upload

---
+++ factorial_1.fs
@@ -0,0 +1,16 @@
+﻿let factorial n =
+    //Incorrect argument
+    if n < 1 then
+        -1
+    else
+    let rec factorial n acc =
+        if n > 1 then
+            factorial (n - 1) (acc * n)
+        else
+            acc
+    factorial n 1
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
