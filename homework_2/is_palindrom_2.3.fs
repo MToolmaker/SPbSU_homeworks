19726108c232165940cd2ea293bb85e391429649
Ivan Yarkov <ivan.yarkov@outlook.com>
2018-02-24 19:00:45 +0300

    Update is_palindrom_2.3.fs

--- homework_2/is_palindrom_2.3.fs
+++ homework_2/is_palindrom_2.3.fs
@@ -2,16 +2,16 @@ let isPalindrom (text: string) =
     if text = "" then
         true
     else
-    let length = text.Length
-    let middleOfText = text.Length / 2
-    let rec isPalindrom i =
-        if i = middleOfText + 1 then
-            true
-        else if text.[i] <> text.[length - i - 1] then
-            false
-        else
-            isPalindrom (i + 1)
-    isPalindrom 0
+        let length = text.Length
+        let middleOfText = text.Length / 2
+        let rec isPalindrom i =
+            if i = middleOfText + 1 then
+                true
+            else if text.[i] <> text.[length - i - 1] then
+                false
+            else
+                isPalindrom (i + 1)
+        isPalindrom 0
     
 let firstTest = isPalindrom "otto"
 let secondTest = isPalindrom "ttto"
