3213f190458ad4e3964a478f4c9c09a9ca23ffb2
Ivan Yarkov <ivan.yarkov@outlook.com>
2018-03-31 20:23:52 +0300

    Update create_sequence_of_prime_numbers_3.4.fs

--- homework_3/create_sequence_of_prime_numbers_3.4.fs
+++ homework_3/create_sequence_of_prime_numbers_3.4.fs
@@ -1,6 +1,6 @@
 let createSequence() = 
     let isPrimeNumber n =
-         [2..n - 1] |> Seq.exists(fun x -> n % x = 0) |> not 
+         [2..n/2] |> Seq.exists(fun x -> n % x = 0) |> not 
     Seq.initInfinite(fun x -> x + 2) |> Seq.filter isPrimeNumber
 
 module ``createSequence tests`` =
