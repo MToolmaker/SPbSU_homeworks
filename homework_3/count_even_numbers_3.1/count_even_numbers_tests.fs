187536a5583afcfb6542601a2e855fa72aabe037
Ivan Yarkov <ivan.yarkov@outlook.com>
2018-04-02 01:49:40 +0300

    Update count_even_numbers_tests.fs

--- homework_3/count_even_numbers_3.1/count_even_numbers_tests.fs
+++ homework_3/count_even_numbers_3.1/count_even_numbers_tests.fs
@@ -1,60 +1,24 @@
-module ``foldEvenNumbers tests`` =
-    open NUnit.Framework
-    open FsUnit
-    open count_even_numbers
-    [<Test>]
-    let ``countEvenNumbers1 [-2; 1; 2; 3; 5; 6] must return 3`` () =
-        foldEvenNumbers [-2; 1; 2; 3; 5; 6] |> should equal 3
+open NUnit.Framework
+[<TestFixture>]
+module ``countEvenNumbers tests`` =
+    open countEvenNumbers
+    let emptyList: int list = []
+    let testData () =
+        [ 
+            TestCaseData(emptyList).Returns(None)
+            TestCaseData([-2; 1; 2; 3; 5; 6]).Returns(Some(3))
+            TestCaseData([1..1000]).Returns(Some(500))
+            TestCaseData([-500..500]).Returns(Some(501))
+        ]
 
-    [<Test>]
-    let ``countEvenNumbers1 [] must return 0`` () =
-        foldEvenNumbers [] |> should equal 0
-    
-    [<Test>]
-    let ``countEvenNumbers1 [1..1000] must return 500`` () =
-        foldEvenNumbers [1..1000] |> should equal 500
-    
-    [<Test>]
-    let ``countEvenNumbers1 [-500..500] must return 501`` () =
-        foldEvenNumbers [-500..500] |> should equal 501
+    [<TestCaseSource("testData")>]
+    let foldCountEvenNumbersTests list = 
+        foldCountEvenNumbers list
 
-module ``filterCountEvenNumbers tests`` =
-    open NUnit.Framework
-    open FsUnit
-    open count_even_numbers
-    [<Test>]
-    let ``countEvenNumbers2 [-2; 1; 2; 3; 5; 6] must return 3`` () =
-        filterCountEvenNumbers [-2; 1; 2; 3; 5; 6] |> should equal 3
+    [<TestCaseSource("testData")>]
+    let filterCountEvenNumbersTests list = 
+        filterCountEvenNumbers list
 
-    [<Test>]
-    let ``countEvenNumbers2 [] must return 0`` () =
-        filterCountEvenNumbers [] |> should equal 0
-    
-    [<Test>]
-    let ``countEvenNumbers2 [1..1000] must return 500`` () =
-        filterCountEvenNumbers [1..1000] |> should equal 500
-    
-    [<Test>]
-    let ``countEvenNumbers2 [-500..500] must return 501`` () =
-        filterCountEvenNumbers [-500..500] |> should equal 501
-
-module ``mapCountEvenNumbers tests`` =
-    open NUnit.Framework
-    open FsUnit
-    open count_even_numbers
-
-    [<Test>]
-    let ``countEvenNumbers3 [-2; 1; 2; 3; 5; 6] must return 3`` () =
-        mapCountEvenNumbers [-2; 1; 2; 3; 5; 6] |> should equal 3
-
-    [<Test>]
-    let ``countEvenNumbers3 [] must return 0`` () =
-        mapCountEvenNumbers [] |> should equal 0
-    
-    [<Test>]
-    let ``countEvenNumbers3 [1..1000] must return 500`` () =
-        mapCountEvenNumbers [1..1000] |> should equal 500
-    
-    [<Test>]
-    let ``countEvenNumbers3 [-500..500] must return 501`` () =
-        mapCountEvenNumbers [-500..500] |> should equal 501
+    [<TestCaseSource("testData")>]
+    let mapCountEvenNumbersTests list = 
+        filterCountEvenNumbers list
