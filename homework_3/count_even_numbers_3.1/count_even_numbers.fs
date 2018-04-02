0801c64e87c9eb12fcb7820cd9a53e4ad8c582a2
Ivan Yarkov <ivan.yarkov@outlook.com>
2018-04-02 20:56:59 +0300

    Update count_even_numbers.fs

--- homework_3/count_even_numbers_3.1/count_even_numbers.fs
+++ homework_3/count_even_numbers_3.1/count_even_numbers.fs
@@ -1,4 +1,4 @@
 module countEvenNumbers
-    let foldCountEvenNumbers list = if list = [] then None else Some(List.fold (fun acc x -> if x % 2 = 0 then acc + 1 else acc) 0 list)
-    let filterCountEvenNumbers list = if list = [] then None else Some(list |> List.filter (fun x -> x % 2 = 0) |> List.length)
-    let mapCountEvenNumbers list = if list = [] then None else Some(List.map (fun x -> if (x % 2) <> 0 then 0 else 1) list |> List.sum)
+    let foldCountEvenNumbers list = List.fold (fun acc x -> if x % 2 = 0 then acc + 1 else acc) 0 list
+    let filterCountEvenNumbers list = list |> List.filter (fun x -> x % 2 = 0) |> List.length
+    let mapCountEvenNumbers list = List.map (fun x -> if (x % 2) <> 0 then 0 else 1) list |> List.sum
