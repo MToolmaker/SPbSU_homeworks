86875984baf6f4713489abc5840b6c71ef2dc447
Ivan Yarkov <ivan.yarkov@outlook.com>
2018-04-03 14:28:55 +0300

    Update find_sin_average.fs
    Задание было прочитано как "найдите синус среднего арифметического", а по факту среднее арифметического синуса каждого числа. Файл поправлен после пары, когда я перечитал задание, то есть уже на ваше усмотрение засчитывать или нет.

--- Test-1/sin_average_test_1/find_sin_average.fs
+++ Test-1/sin_average_test_1/find_sin_average.fs
@@ -1,8 +1,10 @@
 module sinAverage
     let findSinAverage list =
         let floatLength = List.length list |> float
-        let rec findAverage list (acc : float) =
-            match list with
-            | [] -> acc / floatLength
-            | head :: tail -> findAverage tail (acc + head)
-        findAverage list 0.0 |> sin
+        if floatLength < 1.0 then 0.0
+        else
+            let rec findAverage list (acc : float) =
+                match list with
+                | [] -> acc / floatLength
+                | head :: tail -> findAverage tail (acc + sin head)
+            findAverage list 0.0
