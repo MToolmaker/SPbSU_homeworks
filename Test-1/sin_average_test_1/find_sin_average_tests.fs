193f7979adc63d881070ce6892cf69d12c8ca9d1
Ivan Yarkov <ivan.yarkov@outlook.com>
2018-04-03 14:25:34 +0300

    Update find_sin_average_tests.fs

--- Test-1/sin_average_test_1/find_sin_average_tests.fs
+++ Test-1/sin_average_test_1/find_sin_average_tests.fs
@@ -2,11 +2,12 @@ module ``findSinAverage tests`` =
     open NUnit.Framework
     open FsUnit
     open sinAverage
+
     let epsilon = 0.01
 
     [<Test>]
     let ``Sin Average [-1.0 ; 1.0; 3.0 * 1.54] must be about 1`` () =
-        abs ((findSinAverage [-1.0 ; 1.0; 3.0 * 1.54]) - 1) < epsilon |> should equal true
+        abs ((findSinAverage [-1.0 ; 1.0; 3.0 * 1.54]) + 0.33) < epsilon |> should equal true
 
     [<Test>]
     let ``Sin Average [-1.0 ; 0.0; 1.0] must be about 0`` () =
