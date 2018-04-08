653fce3ae0d5bb068696835530c02c2acb09ba87
Ivan <ivan.yarkov@outlook.com>
2018-04-08 22:28:35 +0300

    Implemented first version of 5.1

--- homework_5/check_correctness_5.1/check_correctness_5.1/checkCorrectness.fs
+++ homework_5/check_correctness_5.1/check_correctness_5.1/checkCorrectness.fs
@@ -1,2 +1,44 @@
 ﻿module checkCorrectness
+    type Direction =
+        | Forward
+        | Backward
+    type BraceType =
+        | Parenthesis
+        | SquareBracket
+        | Brace
 
+    let braceChecker (s : string) =
+
+        let toBrace char =
+            match char with
+            | '(' -> Some(Parenthesis, Forward)
+            | ')' -> Some(Parenthesis, Backward)
+            | '[' -> Some(SquareBracket, Forward)
+            | ']' -> Some(SquareBracket, Backward)
+            | '{' -> Some(Brace, Forward)
+            | '}' -> Some(Brace, Backward)
+            | _ -> None
+
+            
+        let rec braceChecker beginIndex endIndex currentBrace direction =
+            if beginIndex > endIndex then
+                if currentBrace = None then
+                    true
+                else
+                    false
+            else
+                if direction = Forward then
+                    let brace = toBrace (s.[beginIndex])
+
+                    match brace with
+                    | Some(_) when snd brace.Value = Backward -> false
+                    | Some(_) -> braceChecker beginIndex endIndex brace Backward
+                    | _ -> braceChecker (beginIndex + 1) endIndex None Forward
+                else
+                    let brace = toBrace (s.[endIndex])
+                    
+                    match brace with
+                    | Some(_) when Some(fst currentBrace.Value, Backward) = brace -> braceChecker (beginIndex + 1) (endIndex - 1) None Forward
+                    | Some(_) -> false
+                    | None -> braceChecker beginIndex (endIndex - 1) currentBrace Backward
+        braceChecker 0 (s.Length - 1) None Forward
