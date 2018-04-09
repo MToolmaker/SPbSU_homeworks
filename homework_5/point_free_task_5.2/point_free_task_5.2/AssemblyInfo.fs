3461aa64e30e80404c8b92d80f00969565a47628
Ivan <ivan.yarkov@outlook.com>
2018-04-09 19:36:04 +0300

    Implemented 5.2

---
+++ homework_5/point_free_task_5.2/point_free_task_5.2/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace point_free_task_5._2.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("point_free_task_5.2")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("point_free_task_5.2")>]
+[<assembly: AssemblyCopyright("Copyright ©  2018")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("5cdf3999-399d-4790-9261-b49d990e4eda")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ homework_5/point_free_task_5.2/point_free_task_tests_5.2/pointFreeTaskTests.fs
@@ -0,0 +1,21 @@
+﻿module pointFreeTaskTests
+    open NUnit.Framework
+    open FsCheck
+    open pointFreeTask
+
+    [<Test>]
+    let ``Equality: func'0 and func'1`` () =
+        Check.Quick (fun x l -> func'0 x l = func'1 x l)
+    
+    [<Test>]
+    let ``Equality: func'1 and func'2`` () =
+        Check.Quick (fun x l -> (func'1 x l = func'2 x l))
+    
+    [<Test>]
+    let ``Equality: func'2 and func'3`` () =
+        Check.Quick (fun x l -> (func'2 x l = func'3 x l))
+    
+    [<Test>]
+    let ``Equality: func'3 and func'0`` () =
+        Check.Quick (fun x l -> (func'3 x l = func'0 x l))
+
