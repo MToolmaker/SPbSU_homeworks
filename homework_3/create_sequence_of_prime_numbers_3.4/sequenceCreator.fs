2ecaeecd018ab6f1a7f2e60a4b37ff5865ca25bf
Ivan <ivan.yarkov@outlook.com>
2018-04-09 12:50:57 +0300

    //

--- homework_3/create_sequence_of_prime_numbers_3.4/sequenceCreator.fs
+++
@@ -1,7 +0,0 @@
-module sequenceCreator
-    let createSequence() = 
-        let isPrimeNumber n =
-             let k = float n |> sqrt |> int
-             [2..k + 1] |> Seq.exists(fun x -> n % x = 0) |> not 
-        Seq.initInfinite(fun x -> x + 2) |> Seq.filter isPrimeNumber
-      
