6d0e0d33b2103171b1bf0b3513cc9ba99a84c26c
Ivan <ivan.yarkov@outlook.com>
2018-04-09 13:11:52 +0300

    Revert "//"
    This reverts commit ba90f3641759df1bd4ebc178002dc7786a69711b.
    
    # Conflicts:
    #	homework_3/Homework_3/count_even_numbers_3.1_tests/App.config
    #	homework_3/Homework_3/count_even_numbers_3.1_tests/AssemblyInfo.fs
    #	homework_3/Homework_3/count_even_numbers_3.1_tests/countEvenNumbersTests.fs
    #	homework_3/Homework_3/count_even_numbers_3.1_tests/count_even_numbers_3.1_tests.fsproj
    #	homework_3/Homework_3/count_even_numbers_3.1_tests/packages.config

--- homework_3/Homework_3/sequence_of_prime_numbers_3.4/sequenceGenerator.fs
+++
@@ -1,6 +0,0 @@
-﻿module sequenceGenerator
-    let createSequence() = 
-        let isPrimeNumber n =
-            let k = float n |> sqrt |> int
-            [2..k] |> Seq.exists(fun x -> n % x = 0) |> not 
-        Seq.initInfinite(fun x -> x + 2) |> Seq.filter isPrimeNumber
