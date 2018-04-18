08201a5a6b9bc4034ad53ff913ecfb7656602fc9
Ivan <ivan.yarkov@outlook.com>
2018-04-18 16:39:26 +0300

    Added testing means to type network like InfectedIDs and so on. Added new tests.

--- homework_6/Network/Network/NetworkTask.fs
+++ homework_6/Network/Network/NetworkTask.fs
@@ -24,23 +24,21 @@
         member val Infected = isInfected with get, set
 
         member this.IsConnected(id) =
-            let rec IsConnected (list : Computer list) =
-                match list with
-                | [] -> false
-                | head :: tail when head.ID <> id -> IsConnected tail
-                | _ -> true
-            IsConnected adjacentComputers
+            List.exists (fun (comp : Computer) -> comp.ID = id) adjacentComputers
 
         member this.Connect (a : Computer) =
             if this.ID <> a.ID && not (this.IsConnected a.ID) then
                 adjacentComputers <- a :: adjacentComputers
                 a.Connect(this)
 
+
+    
     type Network(name : string, getInfectionProbability : OS -> float) =
         let mutable nodes : Computer list = []
         let mutable turn = 0
         let rand = new Random()
 
+        let showIds list = List.map (fun id -> printfn "ID: %d" id) list |> ignore
         let infectConnectedComputers list =
             list 
             |> List.filter (fun (comp : Computer) -> rand.NextDouble() < getInfectionProbability(comp.OS))
@@ -48,9 +46,9 @@
             |> ignore
         
         member val Name = name with get
-        member this.Turn with get() = turn
 
-        //if there is computer in network with given id it will return false, otherwise true.
+        member __.Turn with get() = turn
+
         member this.AddComputer id os isInfected =
             if List.exists (fun (comp : Computer) -> comp.ID = id) nodes then
                 false
@@ -66,30 +64,26 @@
                 true
             with
                 | :? System.Collections.Generic.KeyNotFoundException -> false
-            
 
-
-        member __.ShowAllComputers() =
-            nodes 
-            |> List.map (fun (comp : Computer) -> printfn "ID: %d, isInfected: %b" comp.ID comp.Infected)
-            |> ignore
-
-        member __.ShowCleanComputers() =
-            nodes
-            |> List.filter (fun (comp : Computer) -> not comp.Infected)
-            |> List.map (fun (comp : Computer) -> printfn "ID: %d" comp.ID)
-            |> ignore
-    
-        member __.ShowInfectedComputers() =
-            nodes
-            |> List.filter (fun (comp : Computer) -> comp.Infected)
-            |> List.map (fun (comp : Computer) -> printfn "ID: %d" comp.ID)
-            |> ignore
-
+        member __.IDs with get() = nodes |> List.map (fun (comp : Computer) -> comp.ID)
+        
+        member this.ShowAllComputers() =
+            this.IDs |> showIds
+       
+        member __.CleanIDs with get() = nodes |> List.filter (fun (comp : Computer) -> not comp.Infected) |> List.map (fun (comp : Computer) -> comp.ID)
+        
+        member this.ShowCleanComputers() =
+            this.CleanIDs |> showIds
+        
+        member __.InfectedIDs with get() = nodes |> List.filter (fun (comp : Computer) -> comp.Infected) |> List.map (fun (comp : Computer) -> comp.ID)
+       
+        member this.ShowInfectedComputers() =
+            this.InfectedIDs |> showIds
+        
         member __.TakeTurn() =
-            nodes 
+            nodes
             |> List.filter (fun (comp : Computer) -> comp.Infected)
-            |> List.map (fun (infected : Computer) -> infected.AdjacentComputers |> infectConnectedComputers)
+            |> List.map (fun (infected : Computer) -> infectConnectedComputers infected.AdjacentComputers)
             |> ignore
             turn <- turn + 1
 
--- homework_6/Network/NetworkTests/NetworkTaskTests.fs
+++ homework_6/Network/NetworkTests/NetworkTaskTests.fs
@@ -8,7 +8,7 @@
         let probFun = getInfectionProbability (0.5, 0.2, 0.3)
         let network = new Network("TestNetwork", probFun)
 
-        network.AddComputer 1 OS.Windows true |> ignore
+        network.AddComputer 1 OS.Windows true |> should equal true
         network.AddComputer 1 OS.Linux false |> should equal false
 
     [<Test>]
@@ -16,8 +16,8 @@
         let probFun = getInfectionProbability (0.5, 0.2, 0.3)
         let network = new Network("TestNetwork", probFun)
 
-        network.AddComputer 1 OS.Windows true |> ignore
-        network.AddComputer 2 OS.Linux false |> ignore
+        network.AddComputer 1 OS.Windows true |> should equal true
+        network.AddComputer 2 OS.Linux false |> should equal true
         network.Connect 1 2 |> should equal true
     
     [<Test>]
@@ -25,8 +25,96 @@
         let probFun = getInfectionProbability (0.5, 0.2, 0.3)
         let network = new Network("TestNetwork", probFun)
 
-        network.AddComputer 1 OS.Windows true |> ignore
-        network.AddComputer 2 OS.Linux false |> ignore
+        network.AddComputer 1 OS.Windows true |> should equal true
+        network.AddComputer 2 OS.Linux false |> should equal true
         network.Connect 1 3 |> should equal false
+    
+    [<Test>]
+    let ``Adjacent computers to infected must be infected at every turn. 100% probability of infection for all OS's.``() =
+        let probFun = getInfectionProbability (1.0, 1.0, 1.0)
+        let network = new Network("TestNetwork", probFun)
 
-        
+        network.AddComputer 1 OS.Windows true |> should equal true
+        network.AddComputer 2 OS.Linux false |> should equal true
+        network.AddComputer 3 OS.MacOS false |> should equal true
+        network.AddComputer 4 OS.Windows false |> should equal true
+        network.AddComputer 5 OS.Linux false |> should equal true
+
+        network.Connect 1 2 |> should equal true
+        network.Connect 1 3 |> should equal true
+        network.Connect 2 4 |> should equal true
+        network.Connect 3 4 |> should equal true
+        network.Connect 4 5 |> should equal true
+
+        let infected'0 : Set<int> = set network.InfectedIDs
+        let expectedInfected'0 : Set<int> = set [1]
+        infected'0 |> should equal expectedInfected'0
+
+        network.TakeTurn()
+        let infected'1 : Set<int> = set network.InfectedIDs
+        let expectedInfected'1 = expectedInfected'0.Add(2).Add(3)
+
+        infected'1 |> should equal expectedInfected'1
+
+        network.TakeTurn()
+        let infected'2 : Set<int> = set network.InfectedIDs
+        let expectedInfected'2 = expectedInfected'1.Add(4)
+        infected'2 |> should equal expectedInfected'2
+
+        network.TakeTurn()
+        let infected'3 : Set<int> = set network.InfectedIDs
+        let expectedInfected'3 = expectedInfected'2.Add(5)
+        infected'3 |> should equal expectedInfected'3
+
+    [<Test>]
+    let ``Adjacent computers to infected must be clean at every turn. 0% probability of infection for all OS's.``() =
+        let probFun = getInfectionProbability (0.0, 0.0, 0.0)
+        let network = new Network("TestNetwork", probFun)
+
+        network.AddComputer 1 OS.Windows true |> should equal true
+        network.AddComputer 2 OS.Linux false |> should equal true
+        network.AddComputer 3 OS.MacOS false |> should equal true
+        network.AddComputer 4 OS.Windows false |> should equal true
+        network.AddComputer 5 OS.Linux true |> should equal true
+
+        network.Connect 1 2 |> should equal true
+        network.Connect 1 3 |> should equal true
+        network.Connect 2 4 |> should equal true
+        network.Connect 3 4 |> should equal true
+        network.Connect 4 5 |> should equal true 
+
+        for i in 1..100 do
+            set network.InfectedIDs |> should equal <| set [1; 5]
+            network.TakeTurn()
+
+    [<Test>]
+    let ``Linux and MacOS computers must be clean at every turn. Windows 100%, Linux 0%, MacOs 0%.``() =
+        let probFun = getInfectionProbability (1.0, 0.0, 0.0)
+        let network = new Network("TestNetwork", probFun)
+        //Let's suggest infected computer can be added to a network with OS which has 0% probability of being infected.
+        network.AddComputer 1 OS.Windows false |> should equal true
+        network.AddComputer 2 OS.Linux true |> should equal true
+        network.AddComputer 3 OS.MacOS false |> should equal true
+        network.AddComputer 4 OS.Linux false |> should equal true
+        //This computer will always be clean because there's only one path to it from computer 2 which goes through computer 4 which cannot be infected
+        network.AddComputer 5 OS.Windows false |> should equal true
+
+        network.Connect 1 2 |> should equal true
+        network.Connect 1 3 |> should equal true
+        network.Connect 2 4 |> should equal true
+        network.Connect 3 4 |> should equal true
+        network.Connect 4 5 |> should equal true 
+
+        let infected'0 : Set<int> = set network.InfectedIDs
+        let expectedInfected'0 : Set<int> = set [2]
+        infected'0 |> should equal expectedInfected'0
+
+        network.TakeTurn()
+        let infected'1 : Set<int> = set network.InfectedIDs
+        let expectedInfected'1 = expectedInfected'0.Add(1)
+
+        infected'1 |> should equal expectedInfected'1
+
+        for i in 1..10 do
+            set network.InfectedIDs |> should equal expectedInfected'1
+            network.TakeTurn()
