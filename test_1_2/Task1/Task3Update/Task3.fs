503c956fdc943e1e87278f2c933c3b2f9ad9e437
Ivan <ivan.yarkov@outlook.com>
2018-04-24 11:21:20 +0300

    Small fixes.

---
+++ test_1_2/Task1/Task3Update/Task3.fs
@@ -0,0 +1,14 @@
+﻿module Task3
+    type PriorityQueue<'T>() =
+        let mutable queue : (int * 'T) list = []
+        member this.insert priority value = queue <- (priority, value) :: queue
+        member this.extractMin () =
+            let mutable min = None
+            let rec find list =
+                match list with
+                | [] -> ()
+                | list -> if min = None || (fst <| List.head list ) < (fst min.Value) then min <- Some(List.head list)
+            find queue
+            snd min.Value
+
+
