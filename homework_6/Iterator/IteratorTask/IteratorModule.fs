1012cbbeb1676977f70d2edff8d038a58a96d6ed
Ivan <ivan.yarkov@outlook.com>
2018-04-29 10:36:01 +0300

    Implemented iterator task.

---
+++ homework_6/Iterator/IteratorTask/IteratorModule.fs
@@ -0,0 +1,97 @@
+﻿module IteratorModule
+    open System.Collections.Generic
+    //Immutable binary search tree
+    type Tree<'key, 'value> =
+        | Empty
+        | Node of value: ('key * 'value) * left: Tree<'key, 'value> * right: Tree<'key, 'value>
+        interface IEnumerable<'value> with
+            member this.GetEnumerator(): IEnumerator<'value> = 
+                new TreeEnum<'key, 'value>(this) :> IEnumerator<'value>
+            member this.GetEnumerator(): System.Collections.IEnumerator = 
+                new TreeEnum<'key, 'value>(this) :> System.Collections.IEnumerator
+    
+    and TreeEnum<'key, 'value>(tree : Tree<'key, ' value>) =
+
+        let rec linearize (binTree : Tree<'key, 'value>) =
+            match binTree with
+            | Empty -> []
+            | Node((_, value), left, right) -> (linearize left) @ [value] @ (linearize right)
+        
+        let values = linearize tree
+        let mutable position = -1
+        let length = List.length values
+
+        interface IEnumerator<'value> with
+            member this.Current: 'value =
+                if position < length then
+                    List.item position values
+                else
+                    raise <| System.InvalidOperationException()
+
+            member this.Current: obj = box (this :> IEnumerator<'value>).Current
+                
+            member __.Dispose(): unit =  ()
+
+            member __.MoveNext(): bool =
+                position <- position + 1
+                position < length
+
+            member __.Reset(): unit =
+                position <- -1
+
+
+    let isBinarySearchTree (tree : Tree<'key, 'value>) =
+        let rec IsBinarySearchTree leastKey highestKey tree =
+            match tree with
+            | Empty -> true
+            | Node ((key, _), left, right) ->
+                match leastKey, highestKey with
+                | Some(leastKey), _ when key < leastKey -> false
+                | _, Some(highestKey) when key > highestKey -> false
+                | _ ->
+                    let newHighestValue = Some(defaultArg highestKey key |> min key)
+                    let newLeastValue = Some(defaultArg leastKey key |> max key)
+                    IsBinarySearchTree leastKey newHighestValue left && IsBinarySearchTree newLeastValue highestKey right
+        IsBinarySearchTree None None tree
+    
+    let rec find key (tree : Tree<'a, 'b>) =
+        match tree with
+        | Empty -> None
+        | Node((key', value), _, _) when key = key' -> Some(value)
+        | Node((key', _), left, _) when key < key' -> find key left
+        | Node((key', _), _, right) when key > key' -> find key right
+
+    let rec insert newKey newValue (tree : Tree<'key, 'value>) =
+        match tree with
+        | Empty -> Node ((newKey, newValue), Empty, Empty)
+        | Node ((key, value), left, right) when newKey < key ->
+            let newLeft = insert newKey newValue left
+            Node ((key, value), newLeft, right)
+        | Node ((key, value), left, right) when newKey > key->
+            let newRight = insert newKey newValue right
+            Node ((key, value), left, newRight)
+        | _ -> raise <| System.ArgumentException("Given key already exists.")
+   
+    let rec delete key (tree : Tree<'a, 'b>) =
+        let rec findInOrderPredecessor (tree : Tree<'a, 'b>) =
+            match tree with
+            | Empty -> Empty
+            | Node (_, _, Empty) -> tree
+            | Node (_, _, right) -> findInOrderPredecessor right
+
+        match tree with
+        | Empty -> Empty
+        | Node ((key', value'), left, right) when key < key' ->
+            let left' = delete key left
+            Node ((key', value'), left', right)
+        | Node ((key', value'), left, right) when key > key' ->
+            let right' = delete key right
+            Node ((key', value'), left, right')
+        | Node (_, Empty, Empty) -> Empty
+        | Node (_, left, Empty) -> left
+        | Node (_, Empty, right) -> right
+        | Node (_, left, right) ->
+            let (Node((key', value'), _, _)) = findInOrderPredecessor left
+            let left' = delete key' left
+            Node ((key', value'), left', right)
+
---
+++ homework_6/Iterator/IteratorTests/IteratorModuleTests.fs
@@ -0,0 +1,220 @@
+﻿module IteratorModuleTests
+    open NUnit.Framework
+    open FsUnit
+    open IteratorModule
+    type TestType =
+        {id : int
+         name : string}
+    
+    module IsBinarySearchTreeFunctionTests =
+
+        [<Test>]
+        let ``Checking binary search trees``() =
+            let testTree'1 = Empty
+            isBinarySearchTree testTree'1 |> should equal true
+
+            let testTree'2 = Node((1, 1), Empty, Empty)
+            isBinarySearchTree testTree'2 |> should equal true
+
+            let leftTestTree = Node((0, 0), Empty, Empty)
+            let rightTestTree = Node((2, 2), Empty, Empty)
+
+            let testTree'3 = Node((1, 1), leftTestTree, Empty)
+            isBinarySearchTree testTree'3 |> should equal true
+
+            let testTree'4 = Node((1, 1), Empty, rightTestTree)
+            isBinarySearchTree testTree'4 |> should equal true
+
+            let testTree'5 = Node((1, 1), leftTestTree, rightTestTree)
+            isBinarySearchTree testTree'5 |> should equal true
+        
+        [<Test>]
+        let ``Checking non-search binary trees``() =
+
+            let leftTestTree = Node((0, 0), Empty, Empty)
+            let rightTestTree = Node((2, 2), Empty, Empty)
+
+            let testTree'1 = Node((1, 1), Empty, leftTestTree)
+            isBinarySearchTree testTree'1 |> should equal false
+
+            let testTree'2 = Node((1, 1), rightTestTree, Empty)
+            isBinarySearchTree testTree'2 |> should equal false
+
+            let testTree'3 = Node((10, 1), leftTestTree, rightTestTree)
+            isBinarySearchTree testTree'3 |> should equal false
+
+    module FindFunctionTests =
+
+        [<Test>]
+        let ``Searching by int key in binary search tree``() =
+            let testRecord'1 = {id = 1; name = "John"}
+            let testRecord'2 = {id = 2; name = "David"}
+            let testRecord'3 = {id = 3; name = "Sam"}
+
+            let left = Node((testRecord'1.id, testRecord'1.name), Empty, Empty)
+            let right = Node((testRecord'3.id, testRecord'3.name), Empty, Empty)
+
+            let testTree = Node((testRecord'2.id, testRecord'2.name), left, right)
+        
+            find 0 testTree |> should equal None
+            find 1 testTree |> should equal <| Some(testRecord'1.name)
+            find 2 testTree |> should equal <| Some(testRecord'2.name)
+            find 3 testTree |> should equal <| Some(testRecord'3.name)
+            find 4 testTree |> should equal None
+        
+        [<Test>]
+        let ``Searching by string key in binary search tree``() =
+            let testRecord'1 = {id = 1; name = "John"}
+            let testRecord'2 = {id = 2; name = "David"}
+            let testRecord'3 = {id = 3; name = "Sam"}
+
+            let left = Node((testRecord'2.name, testRecord'2.id), Empty, Empty)
+            let right = Node((testRecord'3.name, testRecord'3.id), Empty, Empty)
+
+            let testTree = Node((testRecord'1.name, testRecord'1.id), left, right)
+
+            find "SomeName1" testTree |> should equal None
+            find "John" testTree |> should equal <| Some(testRecord'1.id)
+            find "David" testTree |> should equal <| Some(testRecord'2.id)
+            find "Sam" testTree |> should equal <| Some(testRecord'3.id)
+            find "SomeName2" testTree |> should equal None
+    
+    module InsertFunctionTests =
+
+        [<Test>]
+        let ``Insert into an empty tree``() =
+            let testTree = Empty
+            insert 1 1 testTree |> should equal <| Node((1,1), Empty, Empty)
+
+        [<Test>]
+        let ``Insert value (with highest key than tree has) into a binary search tree``() =
+            let testTree = Node((1,1), Empty, Empty)
+            insert 2 2 testTree |> should equal <| Node((1,1), Empty, Node((2,2), Empty, Empty))
+
+        [<Test>]
+        let ``Insert value (with least key than tree has) into a binary search tree``() =
+            let testTree = Node((1,1), Empty, Empty)
+            insert 0 0 testTree |> should equal <| Node((1,1), Node((0,0), Empty, Empty), Empty)
+        
+        [<Test>]
+        let ``Insert value advanced test 1``() =
+             let leftTree = Node((1,1), Empty, Empty)
+             let testTree = Node((3,3), leftTree, Empty)
+             insert 2 2 testTree |> should equal <| Node((3,3), Node((1,1), Empty, Node((2,2), Empty, Empty)), Empty)
+        
+        [<Test>]
+        let ``Insert value advanced test 2``() =
+             let leftTree = Node((1,1), Empty, Empty)
+             let testTree = Node((2,2), leftTree, Empty)
+             insert 0 0 testTree |> should equal <| Node((2,2), Node((1,1), Node((0,0), Empty, Empty), Empty), Empty)
+        
+        [<Test>]
+        let ``Insert value advanced test 3``() =
+             let rightTree = Node((4,4), Empty, Empty)
+             let testTree = Node((2,2), Empty, rightTree)
+             insert 3 3 testTree |> should equal <| Node((2,2), Empty, Node((4,4), Node((3,3), Empty, Empty), Empty))
+
+        [<Test>]
+        let ``Insert value advanced test 4``() =
+             let rightTree = Node((2,2), Empty, Empty)
+             let testTree = Node((1,1), Empty, rightTree)
+             insert 3 3 testTree |> should equal <| Node((1,1), Empty, Node((2,2), Empty, Node((3,3), Empty, Empty)))
+
+    module DeleteFunctionTests =
+
+        [<Test>]
+        let ``Delete from empty tree``() =
+            let testTree : Tree<int, 'value> = Empty
+            delete 0 testTree |> should equal testTree
+            
+        [<Test>]
+        let ``Delete from binary search tree test 1``() =
+            let leftTestTree = Node((0, 0), Empty, Empty)
+            let rightTestTree = Node((2, 2), Empty, Empty)
+            let testTree = Node((1, 1), leftTestTree, rightTestTree)
+            delete 0 testTree |> should equal <| Node((1, 1), Empty, rightTestTree)
+            delete 2 testTree |> should equal <| Node((1, 1), leftTestTree, Empty)
+            testTree |> delete 0 |> delete 2 |> should equal <| Node((1, 1), Empty, Empty)
+        
+
+        [<Test>]
+        let ``Delete from binary search tree test 2``() =
+            let leftTestTree = Node((0, 0), Empty, Empty)
+            let rightTestTree = Node((2, 2), Empty, Empty)
+            let testTree = Node((1, 1), leftTestTree, rightTestTree)
+            delete 0 testTree |> should equal <| Node((1, 1), Empty, rightTestTree)
+            delete 2 testTree |> should equal <| Node((1, 1), leftTestTree, Empty)
+            testTree |> delete 0 |> delete 2 |> should equal <| Node((1, 1), Empty, Empty)
+            delete 1 testTree |> should equal <| Node((0, 0), Empty, Node((2, 2), Empty, Empty))
+        
+        [<Test>]
+        let ``Delete from binary search tree test 3``() =
+            let testTree = Node((0,0), Empty, Node((1,1), Empty, Node((2,2), Empty, Empty)))
+            delete 0 testTree |> should equal <| Node((1, 1), Empty, Node((2, 2), Empty, Empty))
+            delete 1 testTree |> should equal <| Node((0, 0), Empty, Node((2, 2), Empty, Empty))
+            delete 2 testTree |> should equal <| Node((0, 0), Empty, Node((1, 1), Empty, Empty))
+        
+        [<Test>]
+        let ``Delete from binary search tree test 4``() =
+            let rightTestSubTree = Node((1, 1), Empty, Empty)
+            let leftTestTree = Node((0, 0), Empty, rightTestSubTree)
+            let rightTestTree = Node((3, 3), Empty, Empty)
+            let testTree = Node((2, 2), leftTestTree, rightTestTree)
+            delete 0 testTree |> should equal <| Node((2, 2), Node((1, 1), Empty, Empty), Node((3, 3), Empty, Empty))
+            delete 1 testTree |> should equal <| Node((2, 2), Node((0, 0), Empty, Empty), Node((3, 3), Empty, Empty))
+            delete 2 testTree |> should equal <| Node((1, 1), Node((0, 0), Empty, Empty), Node((3, 3), Empty, Empty))
+
+    module IteratorInterfaceTests =
+        [<Test>]
+        let ``Iterate through empty tree``() =
+            let mutable flag = true
+            for node in Empty do
+                flag <- false
+            flag |> should equal true
+        
+        [<Test>]
+        let ``Iterate through one-element tree``() =
+            let expectedValue = Some(1)
+            let mutable actualValue = None
+            let testTree = Node((1,1), Empty, Empty)
+            for node in testTree do
+                actualValue <- Some(node)
+            actualValue |> should equal expectedValue
+        
+        [<Test>]
+        let ``Iterate through three-element binary search tree``() =
+            let expectedListOfValues = [1; 2; 3]
+            let mutable actualRevertedListOfValues = []
+            let leftTree = Node((1,1), Empty, Empty)
+            let rightTree = Node((3,3), Empty, Empty)
+            let testTree = Node((2,2), leftTree, rightTree)
+            for node in testTree do
+                actualRevertedListOfValues <- node :: actualRevertedListOfValues
+            let actualListOfValues = List.rev actualRevertedListOfValues
+            actualListOfValues |> should equal expectedListOfValues
+        
+        [<Test>]
+        let ``Iterate through degenerate binary search tree``() =
+            let expectedListOfValues = [1; 2; 3]
+            let mutable actualRevertedListOfValues = []
+            let testTree'1 = Node((3,3), Empty, Empty)
+            let testTree'2 = Node((2,2), Empty, testTree'1)
+            let testTree'3 = Node((1,1), Empty, testTree'2)
+            for node in testTree'3 do
+                actualRevertedListOfValues <- node :: actualRevertedListOfValues
+            let actualListOfValues = List.rev actualRevertedListOfValues
+            actualListOfValues |> should equal expectedListOfValues
+        
+        [<Test>]
+        let ``Iterate through binary search tree. Advanced test.``() =
+            let expectedListOfValues = [0; 1; 2; 3]
+            let mutable actualRevertedListOfValues = []
+            let rightTestSubTree = Node((1, 1), Empty, Empty)
+            let leftTestTree = Node((0, 0), Empty, rightTestSubTree)
+            let rightTestTree = Node((3, 3), Empty, Empty)
+            let testTree = Node((2, 2), leftTestTree, rightTestTree)
+            for node in testTree do
+                actualRevertedListOfValues <- node :: actualRevertedListOfValues
+            let actualListOfValues = List.rev actualRevertedListOfValues
+            actualListOfValues |> should equal expectedListOfValues
+        
