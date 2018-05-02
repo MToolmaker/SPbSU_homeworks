f7214e3beede7cd53c7df9c44ee693b0b0c8193d
Ivan <ivan.yarkov@outlook.com>
2018-05-02 18:07:48 +0300

    Implemented 7.2

---
+++ homework_7/ComputationExpression7.2/Task7.2Tests/Tests.fs
@@ -0,0 +1,63 @@
+﻿module Tests
+    open NUnit.Framework
+    open FsUnit
+    open Builder
+
+    [<Test>]
+    let ``"1"+"2" should be Some(3)``() =
+        let maybe = new MaybeBuilder()
+        let expectedResult = Some(3)
+        let actualResult = maybe{
+            let! a = "1"
+            let! b = "2"
+            return a + b
+        }
+        actualResult |> should equal expectedResult
+    
+    [<Test>]
+    let ``"a"+"2" should be None``() =
+        let maybe = new MaybeBuilder()
+        let expectedResult = None
+        let actualResult = maybe{
+            let! a = "a"
+            let! b = "2"
+            return a + b
+        }
+        actualResult |> should equal expectedResult
+    
+    [<Test>]
+    let ``"1"+"b" should be None``() =
+        let maybe = new MaybeBuilder()
+        let expectedResult = None
+        let actualResult = maybe{
+            let! a = "1"
+            let! b = "b"
+            return a + b
+        }
+        actualResult |> should equal expectedResult
+    
+    [<Test>]
+    let ``"1"+2+"3" should be Some(6)``() =
+        let maybe = new MaybeBuilder()
+        let expectedResult = Some(6)
+        let actualResult = maybe{
+            let! a = "1"
+            let b = 2
+            let! c = "3"
+            return a + b + c
+        }
+        actualResult |> should equal expectedResult
+    
+    [<Test>]
+    let ``"1"+2+"a" should be None``() =
+        let maybe = new MaybeBuilder()
+        let expectedResult = None
+        let actualResult = maybe{
+            let! a = "1"
+            let b = 2
+            let! c = "a"
+            return a + b + c
+        }
+        actualResult |> should equal expectedResult
+
+
