05598a1d46d865115842bf2e332eecdc4f7dea53
Ivan <ivan.yarkov@outlook.com>
2018-05-02 12:12:40 +0300

    Implemented 7.1

---
+++ homework_7/ComputationExpression7.1/Task7.1/Task.fs
@@ -0,0 +1,16 @@
+﻿module Rounding
+    open System
+
+    let (/) a b = if b = 0.0 then None else Some(a/b)
+
+    type MaybeRoundingBuilder(precision : int) =
+        do if precision <= 0 then raise <| System.ArgumentException("Precision can't be " + string precision)
+        member this.Bind(x : float option, f) =
+            match x with
+            | None -> x
+            | Some a ->
+                Math.Round(a,precision) |> f
+        
+        member this.Return(x : float) = Math.Round(x,precision) |> Some
+
+    let rounding precision = new MaybeRoundingBuilder(precision)
