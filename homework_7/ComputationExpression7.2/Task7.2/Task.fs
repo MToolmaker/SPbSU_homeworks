705efe70c94cdb57879dfb8e1ea905ddd07ffa59
Ivan Yarkov <ivan.yarkov@outlook.com>
2018-06-05 13:19:00 +0300

    Update Task.fs

--- homework_7/ComputationExpression7.2/Task7.2/Task.fs
+++ homework_7/ComputationExpression7.2/Task7.2/Task.fs
@@ -2,7 +2,7 @@
      //Let's use an active pattern like this c:
     let (|Int|_|) str =
         match System.Int32.TryParse(str) with
-        | (true,int) -> Some(int)
+        | (true, int) -> Some(int)
         | _ -> None
     
     type MaybeBuilder() =
